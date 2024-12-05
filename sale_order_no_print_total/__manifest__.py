33e7f72f876cd2c42ce7adf436cc2740003f011d
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-12-05 12:36:33 +0100

    initial upload

---
+++ sale_order_no_print_total/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Sale Order No Print Total',
+    'version': '18.0.0.0',
+    'summary': 'Sale Order No Print Total',
+    'description': """
+        Sale Order No Print Total.
+    """,
+    'author': 'Xtendoo',
+    'website': 'https://github.com/xtendoo-corporation',
+    'category': 'Custom',
+    'depends': [
+        'base',
+        'sale',
+    ],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
