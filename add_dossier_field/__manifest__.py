70314e890ba8b0e0f8864f963806f581f71ed44e
dani <dani.xtendoo@gmail.com>
2025-01-08 11:34:14 +0100

    [ADD] add_dossier_field, bug fix torres_document_format

---
+++ add_dossier_field/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Add Dossier Field",
+    "summary": """Añade el campo expediente en factura de clientes""",
+    "version": "18.0.0.0",
+    "description": """Añade el campo expediente en factura de clientes""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_move_views.xml",
+        "views/report_account_move.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ add_dossier_field/models/account_move.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    dossier = fields.Char(
+        string="Expediente",
+    )
