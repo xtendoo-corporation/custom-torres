fb9f53ac7b1326e0d73ce92c4c7acde71d08b2de
dani <dani.xtendoo@gmail.com>
2025-01-08 09:23:46 +0100

    [ADD] torres_document_format

---
+++ torres_document_format/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Document Format Torres",
+    "summary": """Formatos de documentos para Torres""",
+    "version": "18.0.0.0",
+    "description": """Formatos de documentos para Torres""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "web",
+    ],
+    "data": [
+        "views/invoice/external_layout_boxed_invoice.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
